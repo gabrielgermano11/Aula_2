28f52d9947ad9a0dc05cc97822178cf88539c710
gabrielgermano11 <gabrielgermano901@gmail.com>
2026-02-28 14:58:56 +0000

    Aula 2

---
+++ calculadora.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main (void){
+    double a, b, res;
+
+    cout << "Escreva os numeros que você quer dividir?? : ";
+    cin >> a >> b;
+
+    res = a / b;
+
+    cout << "A res é " << res << endl;
+
+    return 0;
+}
---
+++ maior_menor(n2)/maior_menor_2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+   int a, b;
+
+   cout << "Me diga dois numeros: "cd
+   cin >> a >> b;
+
+   if (a < b){
+    cout << a <<" maior que " << b << "\n";
+   }
+    else if (a > b){
+        cout << a << " menor que " << b << "\n";
+    }
+    else{
+        cout << a << " igual a " << b << "\n"; 
+    }
+    
+    return 0
+}
