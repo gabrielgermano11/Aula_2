d18ceeb5e0cdac0acd8b9ce130917db2ec098c49
gabrielgermano11 <gabrielgermano901@gmail.com>
2026-03-14 14:36:22 +0000

    Aula 4

---
+++ vetores/matriz.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(void){
+    int N, M;
+    cin >> N >> M;
+ 
+    int D[N][3]
+    
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < 3; j++){
+            cin >> D[i][j];
+        
+        }
+    }
+
+    for(int i = 0; i< N; i++){
+        for(int j = 0;j < 3; j++){
+            if(j == 1){
+                total
+            }
+        }
+
+    }
+
+    return 0;
+}
---
+++ vetores/vetores.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main (void){
+    int n;
+    cout << "quantos inteiros vc vai escrever";
+    cin << n;
+
+    int v[n];
+    for(int i = 0; i < n; i++){
+        cin >> v[i] ;
+    }
+   
+    cout << i;
+    
+    
+    return 0;
+}
